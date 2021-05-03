ec2505c997298249092fa114771d80a3e7fa3664
zmeyer44 <meyerz@wustl.edu>
2021-05-03 10:54:14 -0500

    tag added

--- packages/pagerland-gatsby/gatsby-config.js
+++ packages/pagerland-gatsby/gatsby-config.js
@@ -50,8 +50,33 @@ module.exports = {
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "270829075",
+        trackingId: '270829075',
         head: false,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          'G-P7JTH3BWWR', // Google Analytics / GA// Marketing Platform advertising products (Display & Video 360, Search Ads 360, and Campaign Manager)
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: 'OPT_CONTAINER_ID',
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ['/preview/**', '/do-not-track/me/too/'],
+        },
       },
     },
     'gatsby-plugin-offline',
